c5778ea9b82eed97c3d477b1a9ff4df8885eb54e
Kshitiz Agrawal <kshitiz.agrawal@finbox.in>
2025-01-25 17:42:34 +0530

    chore: add logger

--- server/logger.go
+++ server/logger.go
@@ -8,68 +8,65 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type logger struct {
+type Logger struct {
 	*logrus.Entry
 }
 
 var (
-	GlobalLogger *logger
+	GlobalLogger *Logger
 )
 
-type loggerContextKeyType string
+const LoggerContextKey = "logger"
 
-const loggerContextKey loggerContextKeyType = "logger"
-
-func init() {
+func NewProductionLogger() *Logger {
 	Log := logrus.New()
 	Log.SetReportCaller(true)
 	Log.SetOutput(os.Stdout)
 	Log.SetFormatter(&logrus.JSONFormatter{})
 	Log.SetLevel(logrus.DebugLevel)
 
-	GlobalLogger = &logger{
+	GlobalLogger = &Logger{
 		Entry: logrus.NewEntry(Log),
 	}
+
+	return GlobalLogger
+}
+
+func newLogger() *Logger {
+	newLogger := *GlobalLogger
+	return &newLogger
+}
+
+// Helper to get logger from context
+func loggerFromContext(ctx context.Context) *Logger {
+	if logger, ok := ctx.Value(LoggerContextKey).(*Logger); ok {
+		return logger
+	}
+
+	return newLogger()
+}
+
+func (logger *Logger) WithTraceID(traceID string) *Logger {
+	logger.Entry = logger.WithField("trace-id", traceID)
+	return logger
 }
 
 // Middleware to extract trace ID and attach a logger with traceID to context
 func withTraceID(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		logger := loggerFromContext(r.Context())
-
 		traceID := r.Header.Get("X-Trace-ID")
 
+		log := loggerFromContext(r.Context())
+
 		// Create a new entry with the fields and assign it back to the logger
-		loggerWithFields := logger.WithFields(logrus.Fields{
-			"trace-id": traceID,
-		})
 
 		if traceID != "" {
 			// Only assign a new entry if traceID is present
-			logger.Entry = loggerWithFields
+			log = log.WithTraceID(traceID)
 		}
 
-		ctx := context.WithValue(r.Context(), loggerContextKey, logger)
+		ctx := context.WithValue(r.Context(), LoggerContextKey, log)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
-}
-
-func newLogger() *logger {
-	newLogger := *GlobalLogger
-	return &newLogger
-}
-
-// Helper to get logger from context
-func loggerFromContext(ctx context.Context) *logger {
-	if logger, ok := ctx.Value(loggerContextKey).(*logger); ok {
-		return logger
-	}
-	return newLogger()
-}
-
-// TODO: Remove
-func (logger *logger) WithX(key string, value interface{}) *logger {
-	logger.Entry = logger.WithField(key, value)
-	return logger
 }
